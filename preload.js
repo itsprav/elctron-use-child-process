3798558713a3ebb0a1e3a37426873c6fdfdd9aaa
itsprav <apraveenkumar.bits@gmail.com>
2020-09-03 14:27:47 +0530

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,24 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+window.openGaeme = function() {
+  var child = require('child_process').execFile;
+  var executablePath = "D:\\Games\\GAMEFOLDER\\GAMENAME.exe";
+  child(executablePath, function(err, data) {
+  if(err){
+  console.error(err);
+  return;
+  }
+  console.log(data.toString());
+  });
+};
